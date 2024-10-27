a418a03c588cbf8e7e921c8ba37bc4b07368ac5e
seisinko <alo.cortez@duocuc.cl>
2024-10-27 16:08:26 -0300

    login page ts
    cambios en el login ts para que sea funcional

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  authType: string = 'login';  
+  loginData = { email: '', password: '' };
+  registerData = { username: '', email: '', password: '' };
 
-  constructor() { }
+  constructor(private router: Router, private toastController: ToastController) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async onLogin() {
+    
+    if (this.loginData.email === 'user@example.com' && this.loginData.password === 'password123') {
+      const toast = await this.toastController.create({
+        message: 'Inicio de sesión exitoso.',
+        duration: 2000,
+        position: 'top',
+      });
+      toast.present();
+      this.router.navigate(['/home']); 
+    } else {
+      const toast = await this.toastController.create({
+        message: 'Correo o contraseña incorrectos.',
+        duration: 2000,
+        position: 'top',
+      });
+      toast.present();
+    }
   }
 
+  async onRegister() {
+    
+    if (this.registerData.username && this.registerData.email && this.registerData.password) {
+      const toast = await this.toastController.create({
+        message: 'Cuenta creada exitosamente.',
+        duration: 2000,
+        position: 'top',
+      });
+      toast.present();
+      this.router.navigate(['/home']); 
+    } else {
+      const toast = await this.toastController.create({
+        message: 'Por favor, completa todos los campos.',
+        duration: 2000,
+        position: 'top',
+      });
+      toast.present();
+    }
+  }
 }
